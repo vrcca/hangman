ab6218fbe548388689339525ac73c3ad1bb5eb2b
Vitor Albuquerque <1920397+vrcca@users.noreply.github.com>
2018-12-01 18:27:19 -0200

    Hides GenServer implementation behind a API

--- hangman/lib/hangman/server.ex
+++ hangman/lib/hangman/server.ex
@@ -1,18 +1,23 @@
 defmodule Hangman.Server do
 
   use GenServer
+  alias Hangman.Game
 
   def start_link() do
     GenServer.start_link(__MODULE__, nil)
   end
 
   def init(_) do
-    { :ok, Hangman.new_game() }
+    { :ok, Game.new_game() }
   end
 
   def handle_call({ :make_move, guess }, _from, state_game) do
-    { game, tally } = Hangman.make_move(state_game, guess)
+    { game, tally } = Game.make_move(state_game, guess)
     { :reply, tally, game }
+  end
+
+  def handle_call({ :tally }, _from, game) do
+    { :reply, Game.tally(game), game }
   end
 
 end
