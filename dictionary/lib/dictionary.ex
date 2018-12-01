5c9749dd6bc3c9d0eb266c0c7bba6f08aa6ff09d
Vitor Albuquerque <1920397+vrcca@users.noreply.github.com>
2018-12-01 15:20:11 -0200

    Turns dictionary library into an application with its own state

--- dictionary/lib/dictionary.ex
+++ dictionary/lib/dictionary.ex
@@ -2,6 +2,5 @@ defmodule Dictionary do
 
   alias Dictionary.WordList
 
-  defdelegate start(), to: WordList, as: :start_link
-  defdelegate random_word(agent), to: WordList
+  defdelegate random_word(), to: WordList
 end
