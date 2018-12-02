9f22d5de04cc7a16b0e77308950e295971ed70dd
Vitor Albuquerque <1920397+vrcca@users.noreply.github.com>
2018-12-02 15:09:03 -0200

    Fix Mover to receive just the tally

--- text_client/lib/text_client/mover.ex
+++ text_client/lib/text_client/mover.ex
@@ -1,7 +1,9 @@
 defmodule TextClient.Mover do
   alias TextClient.State
+
   def make_move(game) do
-    { gs, tally } = Hangman.make_move(game.game_service, game.guess)
-    %State{ game | game_service: gs, tally: tally }
+    tally = Hangman.make_move(game.game_service, game.guess)
+    %State{ game | tally: tally }
   end
+
 end
