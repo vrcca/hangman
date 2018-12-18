b0108642ea6b5d9acfb432b9fee20689084a2ead
Vitor Albuquerque <1920397+vrcca@users.noreply.github.com>
2018-12-18 21:53:08 -0200

    Adds socket javascript support and removes clutter

---
+++ socket_gallows/assets/js/hangman_socket.js
@@ -0,0 +1,9 @@
+import {Socket} from "phoenix"
+
+export default class HangmanSocket {
+
+    constructor() {
+        this.socket = new Socket("/socket", {})
+        this.socket.connect()
+    }
+}
